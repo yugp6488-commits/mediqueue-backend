5af0a8d5ddc85c7faae1ccddb72f3b63714b777f
yugp6488-commits <yug.p6488@gmail.com>
2026-04-14 12:34:57 +0530

    fix: corrected CORS origin typo and regex format

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cors({
     "http://localhost:3001",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
-    "hmedicore-eg7u87sqw-yugp6488-4479s-projects.vercel.app",
+    "medicore-eg7u87sqw-yugp6488-4479s-projects.vercel.app",
     /https:\/\/medicore-.*\.vercel\.app$/,
   ],
   credentials: true,
